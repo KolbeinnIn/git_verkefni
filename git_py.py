563448c41fda587470db17c90cd1cc6e5cf82742
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2017-01-25 12:19:27 +0000

    Dæmi 1 py

---
+++ git_py.py
@@ -0,0 +1,12 @@
+#Git forritun py
+#Kolbeinn
+
+#Dæmi 1:
+tala1 = int(input("Sláðu inn tölu: "))
+tala2 = int(input("Sláðu inn tölu: "))
+summa = tala1 + tala2
+margf = tala1 * tala2
+print(tala1,"+",tala2,"=",summa)
+print(tala1,"*",tala2,"=",margf)
+
+
